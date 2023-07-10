22f7f57564abdad9b6cd7afec9b1e58fcd109a9f
luke <luke.nadackapadam@gmail.com>
2023-07-10 14:10:56 -0500

    Add initial seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# songs = Song.create(title: "Numb", album: "Meteora", artist: "Linkin Park", year: "2003")
+
+# songs = Song.create(title: "Beat It", album: "Thriller", artist: "Michael Jackson", year: "1982")
